6dc70be09002056a6dc5ed46f24dc15f8cf8b15e
Vivek Raman <me@vivekraman.dev>
2025-06-07 21:56:19 -0700

    Fix breadcrumbs

--- src/renderer-editor/components/ShortcutPreview.tsx
+++ src/renderer-editor/components/ShortcutPreview.tsx
@@ -1,6 +1,7 @@
 import { type MantineStyleProp, Text } from '@mantine/core';
-import { Shortcut } from '../../types/Shortcut';
 import ShortcutType from '../../common/enums/ShortcutType';
+import { Shortcut } from '../../types/Shortcut';
+import Icon from './Icon';
 
 interface ShortcutPreviewProps {
   shortcut: Shortcut;
@@ -10,15 +11,18 @@ export default function ShortcutPreview(props: ShortcutPreviewProps) {
   const { shortcut } = props;
   const style: MantineStyleProp = { fontSize: 'smaller' };
 
-  if (shortcut.type === ShortcutType.EMPTY) {
-    return <Text style={style}>Empty</Text>;
-  }
-  if (shortcut.type === ShortcutType.CONTAINER) {
-    return <Text style={style}>Container</Text>;
-  }
-  if (shortcut.type === ShortcutType.SCRIPT) {
-    return <Text style={style}>Script</Text>;
-  }
+  const getIcon = () => {
+    if (shortcut.type === ShortcutType.EMPTY) {
+      return <Icon name="fa6-solid:plus" />;
+    }
+    if (shortcut.type === ShortcutType.CONTAINER) {
+      return <Icon name="fa6-solid:folder" />;
+    }
+    if (shortcut.type === ShortcutType.SCRIPT) {
+      return <Text style={style}>Script</Text>;
+    }
+    return <Icon name="fa6-solid:plus" />;
+  };
 
-  return <div>wat</div>;
+  return getIcon();
 }
